bb8650dcec9f9de617d1040d006568cc0fea2340
NathanielCaron <nathaniel.caron22@gmail.com>
2020-06-10 20:09:15 -0400

    Added completed code and documentation

---
+++ As1/Stack.c
@@ -0,0 +1,253 @@
+/*******************************************************
+ *	Stack.c
+ *
+ *
+ *	Created by Jean-Philippe Legault and Nathaniel Caron
+ ******************************************************/
+
+// allows the usage of `scanf` and `printf`
+#include <stdio.h>
+
+// Has the macro definition for EXIT_SUCCESS
+#include <stdlib.h>
+
+// allows the usage of `bool`
+#include <stdbool.h>
+
+/**
+ * function:
+ *		is_whitespace
+ *
+ * expects:
+ *		a single char
+ *
+ * returns:
+ *  	true when the char is a whitespace character
+ *		false otherwise
+ */
+bool is_whitespace(char in) {
+	return (in == ' ' || in == '\t' || in == '\n' || in == '\r');
+}
+
+/**
+ * function:
+ *		print_stack
+ *
+ * expects:
+ *		a pointer to the root of the stack
+ *		a pointer to the current size of the stack
+ *
+ * Prints a visual representation of the current state of the stack
+ */
+void print_stack(int *stack, int *size) {
+	int i;
+	for(i=0; i<(*size); i++) {
+		printf("|_ %d _|\n", stack[i] );
+	}
+	printf("%d elements\n", (*size) );
+}
+
+/**
+ * function:
+ *		push
+ *
+ * expects:
+ *		pointer to the stack
+ *		pointer to the size
+ *		the value to push
+ *
+ * returns:
+ *  	true when value has been pushed
+ *		false otherwise
+ *
+ * The push function push a value to the passed in stack
+ */
+bool push(int *stack, int *size, int max_size, int to_push) {
+	if((*size) > max_size-1) {
+		return false;
+    }
+    else {
+        *(stack + (*size)) = to_push;
+		(*size)++;
+        return true;
+    }
+}
+
+/**
+ * function:
+ *		pop
+ *
+ * expects:
+ *		pointer to the stack
+ *		pointer to the size
+ *		pointer to location to store the popped value
+ *
+ * returns:
+ *  	true when value has been popped
+ *		false otherwise
+ *
+ * The pop function pops a value from the passed in stack and stores it at the to_return location.
+ */
+bool pop(int *stack, int *size, int *to_return) {
+	if((*size) <= 0) {
+        return false;
+    }
+    else {
+        (*to_return) = *(stack + (*size) - 1);
+        (*size)--;
+		return true;
+    }
+}
+
+/**
+ * function:
+ *		peek
+ *
+ * expects:
+ *		pointer to the stack
+ *		pointer to the size
+ *		pointer to location to store the peeked value
+ *
+ * returns:
+ *  	true when value has been peeked
+ *		false otherwise
+ *
+ * The peek function looks at the top value from the stack and stores it at the to_return location.
+ */
+bool peek(int *stack, int *size, int *to_return) {
+	if((*size) <= 0) {
+		return false;
+	}
+	else {
+		(*to_return) = *(stack + (*size) - 1);
+		return true;
+	}
+}
+
+/**
+ * function:
+ *		main
+ *
+ * expects:
+ *		n/a
+ *
+ * returns:
+ *		EXIT_SUCCESS when program ends.
+ *
+ * while we are not instructed to exit the program
+ *
+ *		We read in a char as an instruction :
+ *			'u' for push
+ *			'o' for pop
+ *			'e' for peek
+ *			'x' to exit the program
+ *
+ *		if the instruction is push ('u'),
+ *			we read in an integer (you may assume it is a valid integer)
+ *			push the read value onto the stack
+ *			if failed
+ *				printf("failed push\n")
+ *			else
+ *				print the value pushed
+ *
+ *		else if the instruction is pop ('o')
+ *			we execute the pop function
+ *			if failed
+ *				printf("failed pop\n")
+ *			else
+ *				print the value popped
+ *
+ *		else if the instruction is peek ('e')
+ *			we execute the peek function
+ *			if failed
+ *				printf("failed peek\n")
+ *			else
+ *				print the value peeked
+ *
+ *		else if the instruction is exit ('x')
+ *			we break out of the loop
+ *
+ *		else
+ *			printf("invalid instruction %c\n", input_instruction);
+ */
+int main() {
+	// keep track of the max size and the current size of the stack
+	int stack_max_size = 5;
+	int stack_current_size = 0;
+
+	// the stack is an array located on the main() function stack frame
+	int stack[stack_max_size];
+
+	// initialize our stack with 0 values
+	int i;
+	for(i=0; i < stack_max_size; i++) {
+		stack[i] = 0;
+	}
+
+	// count the number of instructions (peek, pop, push) that successfully happened
+	int successful_instructions = 0;
+	bool stop_execution = false;
+
+	while(!stop_execution) {
+		// read the input instruction (a single character)
+		char input_instruction = 0;
+		scanf("%c", &input_instruction);
+
+		// the character could be a whitespace so we need to skip those
+		if( false == is_whitespace(input_instruction) ) {
+
+			if(input_instruction == 'u') {
+				int input_value = 0;
+				scanf("%d", &input_value);
+				bool result = push(&stack[0], &stack_current_size, stack_max_size, input_value);
+				if(!result) {
+					printf("failed push\n");
+				}
+ 				else {
+					 printf("%d\n", input_value);
+					 successful_instructions++;
+				}
+
+			}
+
+			else if(input_instruction == 'o') {
+				int to_return = 0;
+				bool result = pop(&stack[0], &stack_current_size, &to_return);
+
+				if(!result) {
+					printf("failed pop\n");
+				}
+ 				else {
+ 					printf("%d\n", to_return);
+					successful_instructions++;
+				}
+			}
+
+			else if(input_instruction == 'e') {
+				int to_return = 0;
+				bool result = peek(&stack[0], &stack_current_size, &to_return);
+				if(!result) {
+					printf("failed peek\n");
+				}
+ 				else {
+					printf("%d\n", to_return);
+					successful_instructions++;
+ 				}
+			}
+
+			else if(input_instruction == 'x') {
+				stop_execution = true;
+			}
+
+			else {
+				printf("invalid instruction %c\n", input_instruction);
+			}
+
+		}
+	}
+
+	printf("Successfully executed %d instructions\n", successful_instructions);
+ 	print_stack(stack, &stack_current_size);
+
+	return EXIT_SUCCESS;
+}
